7b0bdcb7c138a7fbff3b2dafc8f78b3abed99720
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-05-19 10:35:58 -0300

    DepartmentService

---
+++ src/model/services/DepartmentService.java
@@ -0,0 +1,19 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentService {
+	
+	public List<Department> findAll(){
+		//MOCK
+		List<Department> list = new ArrayList<>();
+		list.add(new Department(1, "Music"));
+		list.add(new Department(2, "Food"));
+		list.add(new Department(3, "Books"));
+		return list;
+	}
+	
+}
