3eeda93570ada2c7c4cc311a244bb99123df0d59
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-05-27 20:29:41 -0300

    SellerList

---
+++ src/model/services/SellerService.java
@@ -0,0 +1,29 @@
+package model.services;
+
+import java.util.List;
+
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
+import model.entities.Seller;
+
+public class SellerService {
+	
+	private SellerDAO dao = DAOFactory.createSellerDAO();
+	
+	public List<Seller> findAll(){
+		return dao.findAll();
+	}
+	
+	public void saveOrUpdate(Seller seller) {
+		if(seller.getId() == null) {
+			dao.insert(seller);
+		} else {
+			dao.update(seller);
+		}
+	}
+	
+	public void remove(Seller seller) {
+		dao.deleteById(seller.getId());
+	}
+	
+}
